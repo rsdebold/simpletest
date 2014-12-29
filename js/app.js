e2f0211e14ab88ed2f621461c8cb1b5ce6740eb6
rsdebold <rsdebold@yahoo.com>
2014-12-29 13:23:47 -0600

    initial test
    testing 1st try

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  var activate = ('createTouch' in document) ? 'touchstart' : 'click';
+  $("body > section").first().addClass("current");
+  $("a.back").live(activate, function(event) {
+	var current = $(this).attr("href");
+	$(".current").removeClass("current").addClass("reverse");
+	$(current).addClass("current");
+  });
+  $(".menu a[href]").live(activate, function(event) {
+	var link = $(this), section = link.closest('section'),
+	  prev_element = "#"+(section.removeClass("current").addClass("reverse").attr("id"));
+	$(link.attr("href")).addClass("current");
+	$(".current .back").remove();
+	$(".current .toolbar").prepend("<a href=\""+prev_element+"\" class=\"back\">Back</a>");
+  });
+  alert(1);
+});
